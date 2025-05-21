9394a4830566401d538475fc873185118c3543d1
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-21 16:30:41 +0600

    2 different Fragment created

---
+++ app/src/main/java/com/arsoft/fragmentsapp/FirstFragment.java
@@ -0,0 +1,62 @@
+package com.arsoft.fragmentsapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class FirstFragment extends Fragment {
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        Toast.makeText(context, "onAttach() is Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(getActivity(), "onCreate() is Called", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getActivity(), "onResume() is Called", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Button firstBtn = view.findViewById(R.id.btn_fag1);
+        TextView text = view.findViewById(R.id.text_frag1);
+
+        firstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Welcome to the First Fragment", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+
+    }
+
+}
---
+++ app/src/main/java/com/arsoft/fragmentsapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.arsoft.fragmentsapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+
+    }
+
+
+}
